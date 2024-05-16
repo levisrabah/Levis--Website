787b01858a285236cc311540ab162bce424cd108
levisrabah <levisrabah@gmail.com>
2024-05-17 02:20:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+        var typed = new Typed(".text", {
+            strings: ["FullStack Developer", "Website Designer & Developer", "Tutor" ],
+            typeSpeed: 100,
+            backSpeed: 100,
+            backDelay: 1000,
+            loop: true
+        });
